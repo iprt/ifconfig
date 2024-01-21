eced0b3e8d53f0b9f9e29e4af012bf3f1a7cdd2f
zhenjie zhu <devops@kubectl.net>
2024-01-21 19:09:51 +0800

    Add default values to logging function
    This update assigns the values "default remark" and "default message" to 'log_remark' and 'log_message' respectively if these fields are empty. This ensures the log function will always generate output even when input parameters may be missing.

--- build.sh
+++ build.sh
@@ -4,13 +4,18 @@ SHELL_FOLDER=$(
   cd "$(dirname "$0")"
   pwd
 )
-
 cd "$SHELL_FOLDER"
 
 # shellcheck disable=SC2155
 function log() {
   local log_remark="$1"
   local log_message="$2"
+  if [ -z "$log_remark" ]; then
+    log_remark="default remark"
+  fi
+  if [ -z "$log_message" ]; then
+    log_message="default message"
+  fi
   local current_time=$(date +"%Y-%m-%d %H:%M:%S")
   echo -e "$current_time - [ $log_remark ] $log_message"
 }
